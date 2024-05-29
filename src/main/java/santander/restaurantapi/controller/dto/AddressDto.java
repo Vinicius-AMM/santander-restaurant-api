8839cf078fbb50dda858fea229b8fceff05fa140
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-05-29 16:17:57 -0300

    feat : create AddressDto record

---
+++ src/main/java/santander/restaurantapi/controller/dto/AddressDto.java
@@ -0,0 +1,27 @@
+package santander.restaurantapi.controller.dto;
+
+import santander.restaurantapi.domain.model.Address;
+
+public record AddressDto(Long id, String cep, String city, String state, String neighborhood, String street, String number) {
+    public AddressDto(Address model){
+        this(model.getId(),
+                model.getCep(),
+                model.getCity(),
+                model.getState(),
+                model.getNeighborhood(),
+                model.getStreet(),
+                model.getNumber());
+    }
+
+    public Address toModel(){
+        Address model = new Address();
+        model.setCep(cep);
+        model.setCity(city);
+        model.setState(state);
+        model.setNeighborhood(neighborhood);
+        model.setStreet(street);
+        model.setNumber(number);
+        return model;
+    }
+
+}
