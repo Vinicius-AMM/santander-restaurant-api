0296023e69c6487aa0c64d6bff21d0eac1445cec
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-05-29 16:18:26 -0300

    feat : create BusinessException class

---
+++ src/main/java/santander/restaurantapi/service/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package santander.restaurantapi.service.exception;
+
+public class BusinessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
