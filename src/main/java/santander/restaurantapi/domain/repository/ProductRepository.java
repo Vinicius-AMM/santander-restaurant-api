e3ab658b80b0b543f4a311175f29991c8d4842c1
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-06-03 08:43:25 -0300

    fix : add @Repository annotation

--- src/main/java/santander/restaurantapi/domain/repository/ProductRepository.java
+++ src/main/java/santander/restaurantapi/domain/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package santander.restaurantapi.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import santander.restaurantapi.domain.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
