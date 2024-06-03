8ad857aff39c211f5e2cc03c2c022b3fdf084f9e
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-06-03 08:36:26 -0300

    feat : create the ProductDto record

--- src/main/java/santander/restaurantapi/controller/dto/ProductDto.java
+++ src/main/java/santander/restaurantapi/controller/dto/ProductDto.java
@@ -1,7 +1,28 @@
 package santander.restaurantapi.controller.dto;
 
+import santander.restaurantapi.domain.model.Product;
+
 import java.math.BigDecimal;
 
 public record ProductDto(Long id, String name, String urlPicture, BigDecimal price, String category, String description) {
+    public ProductDto(Product model){
+        this(model.getId(),
+                model.getName(),
+                model.getUrlPicture(),
+                model.getPrice(),
+                model.getCategory(),
+                model.getDescription());
+    }
 
+    public Product toModel(){
+        Product model = new Product();
+        model.setId(id);
+        model.setName(name);
+        model.setName(name);
+        model.setUrlPicture(urlPicture);
+        model.setPrice(price);
+        model.setCategory(category);
+        model.setDescription(description);
+        return model;
+    }
 }
