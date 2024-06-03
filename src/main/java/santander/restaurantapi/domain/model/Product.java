a3be67dcdd584e97589d009e35dfc76e6b55b9b0
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-06-03 08:47:21 -0300

    feat : add implements Serializable to Product class

--- src/main/java/santander/restaurantapi/domain/model/Product.java
+++ src/main/java/santander/restaurantapi/domain/model/Product.java
@@ -3,17 +3,16 @@ package santander.restaurantapi.domain.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "products")
+@Entity(name = "products")
 @Getter
 @Setter
-public class Product extends BaseEntity{
+public class Product extends BaseEntity implements Serializable {
     private BigDecimal price;
     private String category;
     private String description;
