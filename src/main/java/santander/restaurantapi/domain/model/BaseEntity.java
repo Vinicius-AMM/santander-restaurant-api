bd77910d87b83a2882da664d1be40f85885cd77a
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-06-03 08:46:19 -0300

    feat : add implements Serializable to BaseEntity

--- src/main/java/santander/restaurantapi/domain/model/BaseEntity.java
+++ src/main/java/santander/restaurantapi/domain/model/BaseEntity.java
@@ -7,10 +7,12 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @MappedSuperclass
 @Getter
 @Setter
-public abstract class BaseEntity {
+public abstract class BaseEntity  implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
